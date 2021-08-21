b437304d363ec253ae5475945865d43b8d36acb0
Robert Niederreiter <office@squarewave.at>
2021-08-21 21:38:46 +0200

    split up scss files

--- scripts/styles.sh
+++ scripts/styles.sh
@@ -4,4 +4,4 @@ SASS_BIN="./node_modules/sass/sass.js"
 SASS_DIR="./sphinx_conestack_theme/conestack/scss"
 TARGET_DIR="./sphinx_conestack_theme/conestack/static"
 
-$SASS_BIN $SASS_DIR/styles.scss $TARGET_DIR/conestack.css
+$SASS_BIN $SASS_DIR/conestack.scss $TARGET_DIR/conestack.css
