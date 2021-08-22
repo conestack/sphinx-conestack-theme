ecc68cc31ce43a3d6923a323639769d57218bbd4
Robert Niederreiter <office@squarewave.at>
2021-08-22 10:09:49 +0200

    navigation styles and JS WIP

--- sphinx_conestack_theme/conestack/static/conestack.js
+++ sphinx_conestack_theme/conestack/static/conestack.js
@@ -1,16 +1,38 @@
-function toggle_mobile() {
-    if(window.matchMedia('(max-width:768px)').matches) {
-        console.log('mobile');
-        $('#cone-docs-nav').detach().appendTo('#cone-navbar');
-        $('#searchbox').detach().prependTo('#cone-navbar');
-    } else {
-        console.log('desktop');
-        $('#cone-docs-nav').detach().appendTo('#cone-sidebar');
-        $('#searchbox').detach().appendTo('#nav_search');
+let cs = {
+    nav_link_sel: '#cs-docs-nav li.toctree-l1.current a',
+
+    init: function() {
+        $(window).on('resize', cs.toggle_mobile);
+        this.toggle_mobile();
+        this.init_navigation();
+        this.bind_navigation();
+    },
+
+    toggle_mobile: function() {
+        if(window.matchMedia('(max-width:768px)').matches) {
+            $('#cs-docs-nav').detach().appendTo('#cs-navbar');
+            $('#searchbox').detach().prependTo('#cs-navbar');
+        } else {
+            $('#cs-docs-nav').detach().appendTo('#cs-sidebar');
+            $('#searchbox').detach().appendTo('#nav-search');
+        }
+    },
+
+    init_navigation: function() {
+        let anchor = window.location.hash;
+        let curr = $(`${this.nav_link_sel}[href='${anchor}']`);
+        curr.addClass('active');
+    },
+
+    bind_navigation: function() {
+        let nav_links = $(this.nav_link_sel);
+        nav_links.on('click', function(e) {
+            $(nav_links).removeClass('active');
+            $(this).addClass('active');
+        });
     }
 }
 
 $(function() {
-    $(window).on('resize', toggle_mobile);
-    toggle_mobile();
+    cs.init();
 });
